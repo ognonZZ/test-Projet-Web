8cd67cb04e57e89e5034b71bf7fe1625b49edcc1
Zoradik <56223677+Zoradik@users.noreply.github.com>
2022-03-23 10:24:39 +0100

    presentations
    yea

---
+++ presentation stages.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>InternView</title>
+    <link rel="stylesheet" href="./assets/presentation_entreprise.css" class="css">
+    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
+
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/nav&footer.css" class="css">
+
+
+</head>
+<body>
+<!-- Barre de navigation -->
+    <nav>
+        <div class="container-fluid header">
+            <div class="row">
+                <div class="col-lg-2 barnav">
+                    <img class="logo" src="image/logo.png" alt="LOGO">
+                </div>
+                <div class="col-lg-4 barnav">
+                </div>
+                <div class="col-lg-3 barnav">
+                    <input class="style-button" type="button" value="Accueil" onclick="window.location.href='Accueil.php';">
+                    <input class="style-button" type="button" value="Stages" onclick="window.location.href='Stages.php';">
+                    <input class="style-button" type="button" value="Entreprises" onclick="window.location.href='Entreprises.php';">
+                </div>
+                <div class="col-lg-1 barnav">
+                    <i class="fas fa-heart navicon " onclick="window.location.href='https://twitter.com/ognonZZ';"></i>
+                    <i class="fas fa-envelope navicon"onclick="window.location.href='https://twitter.com/ognonZZ';"></i>
+                    <i class="fas fa-bell navicon"onclick="window.location.href='https://twitter.com/ognonZZ';"></i>
+                </div>
+                <div class="col-lg-1 barnav">
+                    <div class="txt">Prénom NOM</div>
+                </div>
+                <div class="col-lg-1 barnav">
+                    <img class="photoprofil" src="image/PP.png" width= "20%" alt="photoprofil" style="border-radius: 50%">
+                </div>
+            </div>
+        </div>
+    </nav>
+<!-- Barre de navigation -->
+
+
+
+<!-- Presentation du stage-->
+<div class="row">
+    
+            <div class="col-6">
+                <div class="txt">
+                <legend>Stage de </legend>
+                 <br>
+                <h2 id="date">Date de l'offre:</h2>
+                 <br>
+                <h3 id="a">Base de rénumération:</h3>
+                 <br>
+                <h3 id="a">Nombres de place disponible:</h3>
+                 <br>
+                <h3 id="a">Nombre de stagiaire déjà accepté:</h3>
+                 <br>
+                <h3 id="a">Domaine de stage:</h3>
+                 <br>
+                <h3 id="a">Adresse mail de contact du stage:</h3>
+                 <br>
+            </div>
+        </div>
+
+
+
+
+    
+            <div class="col-6"> 
+              <div class="scroller"> 
+            <label> Description du stage : <br>
+        </label>
+    </div>
+
+    
+    </div>
+</div>
+
+
+
+
+
+
+<!-- Footer -->
+    <footer class="bg-light text-center text-lg-start">
+        <div class="container-fluid header">
+            <div class="row background">
+                <div class="col-md-1"></div>
+                <div class="col-md-2">
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                        <a href="#!" class="link">A propos de nous</a>
+                        </li>
+                        <li>
+                        <a href="#!" class="link">Nous contacter</a>
+                        </li>
+                        <li>
+                        <a href="#!" class="link">S’inscrire</a>
+                        </li>
+                        <li>
+                        <a href="#!" class="link">Support</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                            <a href="Stages.php" class="link">Stage</a>
+                            </li>
+                            <li>
+                            <a href="Entreprises.php" class="link">Entreprise</a>
+                            </li>
+                            <li>
+                            <a href="#!" class="link">Partenariat</a>
+                            </li>
+                        </ul>
+                </div>
+                <div class="col-md-4"></div>
+                <div class="col-md-3">
+                    <section class="mb-4">
+                        <!-- Facebook -->
+                        <a
+                        class="btn btn-primary btn-floating m-1"
+                        style="background-color: #3b5998;"
+                        href="#!"
+                        role="button"
+                        ><i class="fab fa-facebook-f"></i
+                        ></a>
+                
+                        <!-- Twitter -->
+                        <a
+                        class="btn btn-primary btn-floating m-1"
+                        style="background-color: #55acee;"
+                        href="#!"
+                        role="button"
+                        ><i class="fab fa-twitter"></i
+                        ></a>
+                
+                        <!-- Google -->
+                        <a
+                        class="btn btn-primary btn-floating m-1"
+                        style="background-color: #dd4b39;"
+                        href="#!"
+                        role="button"
+                        ><i class="fab fa-google"></i
+                        ></a>
+                
+                        <!-- Instagram -->
+                        <a
+                        class="btn btn-primary btn-floating m-1"
+                        style="background-color: #ef4b64;"
+                        href="#!"
+                        role="button"
+                        ><i class="fab fa-instagram"></i
+                        ></a>
+                
+                        <!-- Linkedin -->
+                        <a
+                        class="btn btn-primary btn-floating m-1"
+                        style="background-color: #0082ca;"
+                        href="#!"
+                        role="button"
+                        ><i class="fab fa-linkedin-in"></i
+                        ></a>
+                        <!-- Github -->
+                        <a
+                        class="btn btn-primary btn-floating m-1"
+                        style="background-color: #333333;"
+                        href="#!"
+                        role="button"
+                        ><i class="fab fa-github"></i
+                        ></a>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <!-- Copyright -->
+        <div class="text-center p-3 copyright" style="background-color: #3C3F49;">
+          <pre>© 2022    À propos   Accessibilité   Conditions générales d’utilisation de InternView    Politique de confidentialité    Politique relative aux cookies   Politique de copyright</pre>
+        </div>
+        <!-- Copyright -->
+    </footer>
+<!-- Footer -->
+</body>
+</html>
+
